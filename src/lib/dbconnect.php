9202b0bcbec978106da3cf2e2a530a41e0892265
Gungo <gungo06@ufl.edu>
2022-11-02 09:07:33 -0400

    Update dbconnect.php

--- src/lib/dbconnect.php
+++ src/lib/dbconnect.php
@@ -2,8 +2,8 @@
 
 // oci_connect is undefined in my version of php.
 $connection = oci_connect(
-    $username = 'gungo06', 
-    $password = 'mNsfNdleIQgO6L3BFawkpln4',
+    $username = '', 
+    $password = '',
     $connection_string = '//oracle.cise.ufl.edu/orcl');
     //  [//]host_name[:port][/service_name][:server_type][/instance_name]
 
