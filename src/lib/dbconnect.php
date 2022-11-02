e4e95b389a48c7b0414f3ae12d72bda30a3df926
gungo <gungosis@edugenesis.com>
2022-11-02 09:08:34 -0400

    gitignore updated to ignore dbconnect.php

--- src/lib/dbconnect.php
+++ src/lib/dbconnect.php
@@ -2,8 +2,8 @@
 
 // oci_connect is undefined in my version of php.
 $connection = oci_connect(
-    $username = '', 
-    $password = '',
+    $username = 'gungo06', 
+    $password = 'mNsfNdleIQgO6L3BFawkpln4',
     $connection_string = '//oracle.cise.ufl.edu/orcl');
     //  [//]host_name[:port][/service_name][:server_type][/instance_name]
 
